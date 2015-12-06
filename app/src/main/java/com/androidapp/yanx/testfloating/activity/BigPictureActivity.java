17f5f18f99d68c94cf29f558e8a66dbad8355c7d
Yan <typhoon322@qq.com>
2015-12-06 13:56:32 +0800

    Initial commit

---
+++ app/src/main/java/com/androidapp/yanx/testfloating/activity/BigPictureActivity.java
@@ -0,0 +1,7 @@
+package com.androidapp.yanx.testfloating.activity;
+
+/**
+ * Created by pc on 2015/12/6.
+ */
+public class BigPictureActivity {
+}
---
+++ app/src/main/java/com/androidapp/yanx/testfloating/widget/DividerItemDecorator.java
@@ -0,0 +1,7 @@
+package com.androidapp.yanx.testfloating.widget;
+
+/**
+ * Created by pc on 2015/12/6.
+ */
+public class DividerItemDecorator {
+}
